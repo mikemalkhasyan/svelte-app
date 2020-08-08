20bd84837bce3052522442fa7e1103ca49638e29
mikemalkhasyan <mkrtich.malkhasyan1996@gmail.com>
2020-08-08 19:30:22 +0400

    - Creating the Welcome Screen

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,26 @@
 <script>
-	export let name;
+	import Window from "./screens/Window.svelte";
+
+	let state = 'welcome' // or 'playing';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{#if state === 'welcome'}
+		<Window />
+	{:else if state === 'playing'}
+		<p>playing screen goes here</p>
+	{/if}
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		max-width: 800px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
 	}
 </style>
