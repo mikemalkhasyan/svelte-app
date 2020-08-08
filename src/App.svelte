05474b677fb48cb1a381605b9a2737df051e0ae5
mikemalkhasyan <mkrtich.malkhasyan1996@gmail.com>
2020-08-08 20:09:13 +0400

    - Fetching API Data

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,55 @@
 <script>
+	import { onMount } from 'svelte';
 	import Window from "./screens/Window.svelte";
+	import {select} from './select';
+
+	let celebs_promise;
 
 	let state = 'welcome' // or 'playing';
+	let selection;
 
-	const start = (e) => {
-		console.log(e);
+	const start = async (e) => {
+		const { celebs, lookup } = await celebs_promise;
+
+		selection = select(celebs, lookup, e.detail.category.slug);
+		state = `playing`;
 	}
+	const load_celebs = async () => {
+		const res = await fetch('https://cameo-explorer.netlify.app/celebs.json ');
+		const data = await res.json();
+
+		const lookup = new Map();
+
+		data.forEach(c => {
+			lookup.set(c.id, c);
+		});
+
+		const subset = new Set();
+		data.forEach(c => {
+			if (c.reviews >= 50) {
+				subset.add(c);
+				c.similar.forEach(id => {
+					subset.add(lookup.get(id));
+				})
+			}
+		});
+
+		return {
+			celebs: Array.from(subset),
+			lookup
+		}
+	}
+
+	onMount(() => {
+		celebs_promise = load_celebs();
+	});
 </script>
 
 <main>
 	{#if state === 'welcome'}
 		<Window on:select={start} />
 	{:else if state === 'playing'}
-		<p>playing screen goes here</p>
+		<p>game screen goes here</p>
 	{/if}
 </main>
 
